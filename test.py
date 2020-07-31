9344944dad2524f3c7b8ca024f903ab3cdb7f1ad
levy0601 <levy0601@gmail.com>
2020-07-31 12:55:07 -0700

    filter receive data field

--- test.py
+++ test.py
@@ -26,27 +26,53 @@ class listener(StreamListener):
     def on_data(self, data):
         try:
             tweet = json.loads(data)
+
+            convertedTweet = {}
+            convertedTweet['created_at'] = tweet['created_at']
+            convertedTweet['text'] = tweet['text']
+            convertedTweet['user'] = {}
+            convertedTweet['user']['location'] = tweet['user']['location']
+            convertedTweet['geo'] = tweet['geo']
+            convertedTweet['coordinates'] = tweet['coordinates']
+            if tweet.get('place'):
+                convertedTweet['place'] = {}
+                convertedTweet['place']['place_type'] = tweet['place']['place_type']
+                convertedTweet['place']['name'] = tweet['place']['name']
+                convertedTweet['place']['full_name'] = tweet['place']['full_name']
+                convertedTweet['place']['country'] = tweet['place']['country']
+            convertedTweet['reply_count'] = tweet['reply_count']
+            convertedTweet['retweet_count'] = tweet['retweet_count']
+            hasttagArray = []
+            if tweet.get('entities'):
+                if tweet['entities'].get('hashtags'):
+                    for item in tweet['entities']['hashtags']:
+                        hasttagArray.append(item['text'])
+            convertedTweet['hashtags'] = hasttagArray
+
             with open('test_data.json', 'a') as my_file:
-                json.dump(tweet, my_file)
-                print(data)
+                json.dump(convertedTweet, my_file, indent=4)
+                # print("data:")
+                # print(data)
+                print("convertedTweet:")
+                print(json.dumps(convertedTweet))
 
         except BaseException:
             print('Error')
             pass
 
-    def on_status(self, status):
-        print
-        status.text
-        if status.coordinates:
-            print
-            'coords:', status.coordinates
-        if status.place:
-            print
-            'place:', status.place.full_name
-
-        return True
-
-    on_event = on_status
+    # def on_status(self, status):
+    #     print
+    #     status.text
+    #     if status.coordinates:
+    #         print
+    #         'coords:', status.coordinates
+    #     if status.place:
+    #         print
+    #         'place:', status.place.full_name
+    #
+    #     return True
+    #
+    # on_event = on_status
 
     def on_error(self, status):
         print(status)
